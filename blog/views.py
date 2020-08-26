666dc72b7520bfa3a54d4a2f93c598b7878f3765
ticochuck <chucklivilla@gmail.com>
2020-08-26 10:40:05 -0700

    home page completed

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView, TemplateView
+
+from .models import Post
 
 # Create your views here.
+
+class HomePage(ListView):
+    template_name = 'home.html'
+    model = Post
+
