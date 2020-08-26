ddb6461cf8785e9c431d36afb0bbab259e0891c5
ticochuck <chucklivilla@gmail.com>
2020-08-26 16:20:31 -0700

    added basic css

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -125,4 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'blog/static',
+]
+
+STATIC_ROOT = "staticfiles"
 STATIC_URL = '/static/'
